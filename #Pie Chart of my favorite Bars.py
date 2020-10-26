3e6ab50e8d17ecac4d18c5cf5a7dff8e5f62c38c
beltbite <72576465+beltbite@users.noreply.github.com>
2020-10-26 14:00:07 -0500

    Update #Pie Chart of my favorite Bars.py

--- #Pie Chart of my favorite Bars.py
+++ #Pie Chart of my favorite Bars.py
@@ -9,18 +9,18 @@ import numpy as np
 pie_df = pd.read_csv(r"C:\Users\Reape\OneDrive\Desktop\Pie Ranking.csv")
 #print(pie_df)
 
-#labels = pie_df['Pies']
-#plt.figure(figsize=(8, 11))
-#ax = sns.barplot(x= 'Pies', y='Likeness', data=pie_df, order=pie_df.sort_values('Likeness').Pies, hue="Pies", dodge=False)
-#ax.set(xticklabels=[])
-#ax.set(xticks=[])
-#ax.set(xlabel='')
-#plt.title('A Bar Graph of my Favorite Pies')
-#plt.show()
+labels = pie_df['Pies']
+plt.figure(figsize=(8, 11))
+ax = sns.barplot(x= 'Pies', y='Likeness', data=pie_df, order=pie_df.sort_values('Likeness').Pies, hue="Pies", dodge=False)
+ax.set(xticklabels=[])
+ax.set(xticks=[])
+ax.set(xlabel='')
+plt.title('A Bar Graph of my Favorite Pies')
+plt.show()
 
 
 bar_df = pd.read_csv(r"C:\Users\Reape\OneDrive\Desktop\Favbars.csv")
-print(bar_df)
+#print(bar_df)
 bars = bar_df['Favorite Bars']
 likeness = bar_df['Likeness']
 explode=(0,0,0,0,0.1,0)
